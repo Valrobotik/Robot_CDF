08e65b8b2093ab8b8bd71c8f24ff7a356baf6233
ppgg88 <32483270+ppgg88@users.noreply.github.com>
2023-04-20 15:32:04 +0200

    add imu node

--- README.md
+++ README.md
@@ -3,17 +3,15 @@ Ce pakage est une verssion comenté du robot de la coupe de france merci de le g
 
 ### LISTE DES NEUDS :
 - ```serialCon``` situé dans le fichier : script/serialComunication.py
-- ```kalman``` situé dans le fichier : script/kalman.py
+- ```imu``` situé dans le fichier : script/imu.py
 
 ### LISTE DES PUBLISHER/SUBSCRIBER :
 - ```robot_consign``` type : Twist
   - subscribe (serialCon -> setVitConsigneThread.sendConsign())
 - ```server_req``` type : String
   -  subscribe (serialCon -> requestMotorThread.sendReq)
-- ```enc_velocity``` type : Odometry
-  - publish (Kalman)
-- ```diff_velocity``` type : Twist
-
+- ```imu_data``` type : Twist
+  - publish (imu)
 ### LISTE DES SERVICES :
 - ```encoders``` type : encoders
   - service (SerialCon -> getVitThread.handle_encoders)
---
+++ src/robot/script/imu.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# Path: Valrob_pkg\script\imu.py
+# recupere les donees de l'IMU et les publie sur le topic imu_data
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+from BMI160_i2c import Driver
+
+class IMU():
+    def __init__(self) -> None:
+        __adresse = 0x69 #adresse de l'IMU
+        __freq = 100    #frequence de publication des donnees
+        __sensor = Driver(__adresse) #initialisation de l'IMU
+    
+    def run(self):
+        while rospy.is_shutdown() == False: #boucle de publication des donnees tant que le noeud n'est pas arrete
+            data = self.__sensor.get_data() #recuperation des donnees
+            publisher.publish(Twist(Vector3(data[3],data[4],data[5]), Vector3(data[0],data[1],data[2]))) #publication des donnees
+            rospy.sleep(1/self.__freq) #attente de la frequence de publication
+
+imu = IMU() #initialisation de l'IMU           
+rospy.init_node("imu") #initialisation du noeud
+publisher = rospy.Publisher('imu_data', Twist, queue_size=10)   #initialisation du publisher
+imu.run()  #lancement de la boucle de publication
